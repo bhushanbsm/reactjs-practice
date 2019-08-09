1d19ade2e6cd377a4cf683b44f164356d6670a94
bhushan.manusmare <bhushan.manusmare@maximess.com>
2019-08-09 10:43:20 +0530

    fixed conflict

--- src/App.js
+++ src/App.js
@@ -4,22 +4,15 @@ import './App.css';
 import Header from './components/core/Header';
 import Footer from './components/core/Footer';
 import Price from './components/pricing/Index';
-<<<<<<< HEAD
-=======
 import Shop from './components/Shop/Index';
->>>>>>> 3970875dad7573875da31d914a786f18631b57a1
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header></Header>
-<<<<<<< HEAD
-        <Price></Price>
-=======
         {/* <Price></Price> */}
         <Shop />
->>>>>>> 3970875dad7573875da31d914a786f18631b57a1
         <Footer></Footer>
         {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
